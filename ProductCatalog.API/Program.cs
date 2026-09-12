90af09dc2f1aabed06ed35431abca6696ae1e7d5
okusmierek <okusmierek@automotivemastermind.com>
2026-09-12 12:15:00 +0200

    feat: configure CORS for Angular frontend

--- ProductCatalog.API/Program.cs
+++ ProductCatalog.API/Program.cs
@@ -5,8 +5,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<IProductRepository, InMemoryProductRepository>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
+app.UseCors("AllowAngular");
 app.MapProductEndpoints();
 
 app.Run();
